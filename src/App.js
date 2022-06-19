fc7f54e949e14effa6f6e0a12329eba05f0d70ed
Halit Oskan <halit.oskan@and.digital>
2022-06-19 13:57:50 +0100

    Adding new form for multiple inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import FormOne from "./FormComponents/FormOne";
+import BookingForm from "./HotelForms/Bookings";
 
 function App() {
   return (
     <div className="App">
       <FormOne />
+      <BookingForm />
     </div>
   );
 }
---
+++ src/HotelForms/Bookings.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import "./Bookings.css";
+import Input from "./Input.js";
+
+const BookingForm = ({ addNewBooking }) => {
+  const [formValues, setFormValues] = useState({
+    title: "",
+    firstName: "",
+    surname: "",
+    email: "",
+    roomId: "",
+    checkInDate: "",
+    checkOutDate: "",
+  });
+
+  const handleChange = (event) => {
+    const updatedFormValues = {
+      ...formValues,
+      [event.target.name]: event.target.value,
+    };
+
+    setFormValues(updatedFormValues);
+  };
+
+  const displayForm = (formValues) => {
+    console.log(formValues);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // addNewBooking(formValues);
+
+    displayForm(formValues);
+
+    setFormValues({
+      title: "",
+      firstName: "",
+      surname: "",
+      email: "",
+      roomId: "",
+      checkInDate: "",
+      checkOutDate: "",
+    });
+  };
+
+  return (
+    <div className="booking-form">
+      <h3>Book a room</h3>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Title"
+          type="text"
+          name="title"
+          // value={formValues.title} ********* what's difference between value and defaultValue?
+          defaultValue={formValues.title}
+          onChange={handleChange}
+        />
+        <Input
+          label="First name"
+          type="text"
+          name="firstName"
+          // value={formValues.firstName} ********* what's difference between value and defaultValue?
+          defaultValue={formValues.firstName}
+          onChange={handleChange}
+        />
+        <Input
+          label="Surname"
+          type="text"
+          name="surname"
+          value={formValues.surname}
+          onChange={handleChange}
+        />
+        <Input
+          label="Email address"
+          type="text"
+          name="email"
+          value={formValues.email}
+          onChange={handleChange}
+        />
+        <Input
+          label="Room ID"
+          type="number"
+          name="roomId"
+          value={formValues.roomId}
+          onChange={handleChange}
+        />
+        <Input
+          label="Check in date"
+          type="date"
+          name="checkInDate"
+          value={formValues.checkInDate}
+          onChange={handleChange}
+        />
+        <Input
+          label="Check out date"
+          type="date"
+          name="checkOutDate"
+          value={formValues.checkOutDate}
+          onChange={handleChange}
+        />
+        <input type="submit" className="btn btn-primary booking-btn" />
+      </form>
+    </div>
+  );
+};
+
+export default BookingForm;
